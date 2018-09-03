c012a3dc1298991b0be367b8be7f29f581289f24
cya6 <cya6@cornell.edu>
2018-09-03 15:55:26 -0400

    tut

--- sim/tut3_pymtl/sort/MinMaxUnit.py
+++ sim/tut3_pymtl/sort/MinMaxUnit.py
@@ -22,4 +22,10 @@ class MinMaxUnit( Model ):
     # logic here to implement the min/max unit. You should also write a
     # unit test from scratch named MinMaxUnit_test.py.
     # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
+    
+    if s.in0 <= s.in1:
+      s.out_min.value(s.in0.v)
+      s.out_max.value(s.in1.v)
+    else
+      s.out_min.value(s.in1.v)
+      s.out_min.value(s.in0.v)
